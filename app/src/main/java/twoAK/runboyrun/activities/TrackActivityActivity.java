0edbdb648334bacee34fded9c84d0a5441c96018
kazakov24alex <kazakov24alex@gmail.com>
2017-04-23 18:34:23 +0300

    TrackActivity: track activity

--- app/src/main/java/twoAK/runboyrun/activities/TrackActivityActivity.java
+++ app/src/main/java/twoAK/runboyrun/activities/TrackActivityActivity.java
@@ -10,13 +10,22 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.SystemClock;
+import android.speech.tts.TextToSpeech;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.ProgressBar;
+import android.widget.Chronometer;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.lzyzsd.circleprogress.DonutProgress;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -28,67 +37,118 @@ import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.Locale;
 
 import twoAK.runboyrun.R;
 
 
-
-public class TrackActivityActivity extends AppCompatActivity implements OnMapReadyCallback {
-    // The minimum distance to change Updates in meters
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 0; // 5 meters
-    // The minimum time between updates in milliseconds
-    private static final long MIN_TIME_BW_UPDATES = 0; // 1 minutes
-
+public class TrackActivityActivity extends AppCompatActivity implements OnMapReadyCallback, TextToSpeech.OnInitListener {
     private Context ctx = this;
+
+    private TextToSpeech mTTS;
+    private Boolean speechIsAvailable;
+
+    // Activity objects
+    private TextView mDistanceText;
+    private TextView mProviderTitle;
+    private FloatingActionButton mStartButton;
+
+    // locationManager objects
     private LocationManager mLocationManager;
-    
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 0; // The minimum distance to change Updates in meters
+    private static final long MIN_TIME_BW_UPDATES = 0; // The minimum time between updates in milliseconds
+    private double curLat;
+    private double curLon;
+
+    // map objects
     private GoogleMap mGoogleMap;
     private Marker mMarkerCurPos;
     private PolylineOptions rectOptions;
     private Polyline polyline;
-    List<LatLng> routePoints;
 
-    private double lat;
-    private double lon;
-
-
+    // activity attributes
     private boolean isTracked;
+    private Chronometer mChronometer;
+    List<LatLng> mRoutePoints;
 
-    private TextView mProviderTitle;
-    private FloatingActionButton mStartButton;
+    private float mDistance;
+    private float mTempo;
+
+    // alert intervals
+    private int mAlertDistanceInterval;
+    private int mAlertTimeInterval;
+    private int mAlertTempoInterval;
+    private int mAlertDistanceStep;
+    private int mAlertTimeStep;
+    private int mAlertTempoStep;
 
 
-
-    public static ScheduledExecutorService scheduleTaskExecutor = Executors.newScheduledThreadPool(2);
-
-    private void startThreadOnce() {
-
-        scheduleTaskExecutor.scheduleAtFixedRate(
-                new Runnable() {
-
-                    @Override
-                    public void run() {
-                        System.out.println("THREAD:  LAT="+lat+" LON="+lon);
-                    }
-                }, 0, 3, TimeUnit.SECONDS); // 0 >> initial delay, 10 >> every x., TimeUnit.SECONDS >> in seconds
-
-    }
-
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_activity);
+        mTTS = new TextToSpeech(this, this);
+
         isTracked = false;
+        speechIsAvailable = false;
+
+        mAlertDistanceInterval = 0;
+        mAlertTimeInterval = 0;
+        mAlertTempoInterval = 0;
+
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.track_activity_mapview_google);
         mapFragment.getMapAsync(this);
         
         mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        mDistanceText= (TextView) findViewById(R.id.track_activity_text_km_number);
+        mDistanceText.setText(String.format("%.2f", mDistance));
+
+        mChronometer = (Chronometer) findViewById(R.id.track_activity_chronometer);
+        mChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                long elapsedMillis = SystemClock.elapsedRealtime() - mChronometer.getBase();
+
+                if ( (mAlertTimeInterval != 0) && (elapsedMillis > mAlertTimeStep) ) {
+                    int hours = mAlertTimeStep / (1000*60*60);
+                    int minutes = (mAlertTimeStep / (1000*60)) % 60;
+                    Log.i("RUN-BOY-RUN", "[TrackActivity] TimeAlert: HOURS = "+hours+" MINUTES="+minutes);
+
+                    if(hours == 0) {
+                        String speakString = getOrdinalFromNumber(minutes)+" minute of training";
+                        mTTS.speak(speakString, TextToSpeech.QUEUE_ADD, null);
+                    } else if (minutes != 0){
+                        String speakString = " Training lasts "+hours+" hours and "+minutes+" minutes";
+                        mTTS.speak(speakString, TextToSpeech.QUEUE_ADD, null);
+                    }
+
+                    mAlertTimeStep += mAlertTimeInterval * (60*1000);
+                }
+
+                if ( (mAlertTempoInterval != 0) && (elapsedMillis > mAlertTempoStep) ) {
+                    float tempo = 2.524f;
+
+                    int integerPart = (int)Math.floor(tempo);
+                    int fractionalPart = (int)Math.floor((tempo-integerPart)*10);
+                    Log.i("RUN-BOY-RUN", "[TrackActivity] TempoAlert: TEMPO = "+integerPart+"."+fractionalPart);
+
+                    if(mAlertTempoStep != 0) {
+                        if (fractionalPart == 0) {
+                            String speakString = "You pace is " + integerPart + " kilometers per minute";
+                            mTTS.speak(speakString, TextToSpeech.QUEUE_ADD, null);
+                        } else {
+                            String speakString = "You pace is " + integerPart + "." + fractionalPart + " kilometers per minute";
+                            mTTS.speak(speakString, TextToSpeech.QUEUE_ADD, null);
+                        }
+                    }
+
+                    mAlertTempoStep += mAlertTempoInterval * (60*1000);
+                }
+            }
+        });
 
         mProviderTitle = (TextView) findViewById(R.id.track_activity_text_provider_title);
         mStartButton = (FloatingActionButton) findViewById(R.id.track_activity_floatbut_start);
@@ -108,12 +168,15 @@ public class TrackActivityActivity extends AppCompatActivity implements OnMapRea
         if(!checkProvider()) {
             showNotProviderDialog();
         } else {
-            showStartDialog();
+            startActivityTracking();
         }
     }
 
     private void finishTracking() {
-
+        isTracked = false;
+        mChronometer.stop();
+        mStartButton.setBackgroundResource(R.color.GREEN_LIGHT);
+        Toast.makeText(getApplicationContext(), "SEC="+(SystemClock.elapsedRealtime() - mChronometer.getBase()), Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -128,12 +191,12 @@ public class TrackActivityActivity extends AppCompatActivity implements OnMapRea
         rectOptions = new PolylineOptions().color(Color.RED).width(10);
         polyline = mGoogleMap.addPolyline(rectOptions);
 
-        startThreadOnce();
     }
 
 
     @Override
     protected void onResume() {
+        System.out.println("RUN-BOY-RUN : ON RESUME!!!");
         super.onResume();
         mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
                 MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListener);
@@ -159,32 +222,54 @@ public class TrackActivityActivity extends AppCompatActivity implements OnMapRea
             }
 
             if (location.getProvider().equals(LocationManager.GPS_PROVIDER)) {
-
+                // TODO: WHAT FOR
             }
 
             if (location.getProvider().equals(LocationManager.NETWORK_PROVIDER)) {
-
-            }
-
-            lat = location.getLatitude();
-            lon = location.getLongitude();
-
-            System.out.println("LAT = "+ location.getLatitude());
-            System.out.println("LON = "+ location.getLongitude());
-
-            if(rectOptions != null) {
-                LatLng myLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-                List<LatLng> points = polyline.getPoints();
-                points.add(myLatLng);
-                polyline.setPoints(points);
+                // TODO: WHAT FOR
             }
 
 
-            LatLng myplace = new LatLng(location.getLatitude(), location.getLongitude());
+            // getting current position
+            curLat = location.getLatitude();
+            curLon = location.getLongitude();
+            LatLng myplace = new LatLng(curLat, curLon);
 
+            // marker movement
             mMarkerCurPos.setPosition(myplace);
             mMarkerCurPos.setVisible(true);
             mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myplace, 16));
+
+            if(isTracked) {
+                // polyline updating
+                if (rectOptions != null) {
+                    LatLng myLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    List<LatLng> points = polyline.getPoints();
+                    points.add(myLatLng);
+                    polyline.setPoints(points);
+
+                    if (points.size() > 1) {
+                        Location locationA = new Location("point A");
+                        locationA.setLatitude(points.get(points.size() - 2).latitude);
+                        locationA.setLongitude(points.get(points.size() - 2).longitude);
+
+                        System.out.println("RUN-BOY-RUN LAT=" + points.get(points.size() - 2).latitude);
+                        System.out.println("RUN-BOY-RUN LON=" + points.get(points.size() - 2).longitude);
+
+                        Location locationB = new Location("point B");
+                        locationB.setLatitude(curLat);
+                        locationB.setLongitude(curLon);
+
+                        mDistance += locationA.distanceTo(locationB);
+                        mDistanceText.setText(String.format("%.2f", mDistance*0.001));
+
+                        Log.i("RUN-BOY-RUN", "[TrackActivity] TRACKED POSITION: lat=" + curLat + " lon=" + curLon + " DISTANCE=" + locationA.distanceTo(locationB));
+                    }
+                }
+            } else {
+                Log.i("RUN-BOY-RUN", "[TrackActivity] untracked position: lat="+curLat+" lon="+curLon);
+            }
+
 
         }
 
@@ -250,38 +335,148 @@ public class TrackActivityActivity extends AppCompatActivity implements OnMapRea
     }
 
 
-    public void showStartDialog() {
+    public void startActivityTracking() {
+        View settingDialogContent = getLayoutInflater().inflate(R.layout.dialog_setting_activity, null);
 
-        final AlertDialog.Builder startDialog = new AlertDialog.Builder(this);
-        startDialog.setIcon(android.R.drawable.btn_star_big_on);
-        startDialog.setTitle("Начинаем активность!");
-        View dialogContent = getLayoutInflater().inflate(R.layout.dialog_start_activity, null);
-        startDialog.setView(dialogContent);
+        Switch mDistanceAlertSwitcher = (Switch) settingDialogContent.findViewById(R.id.dialog_setting_activity_switch_distance);
+        Switch mTimeAlertSwitcher = (Switch) settingDialogContent.findViewById(R.id.dialog_setting_activity_switch_time);
+        Switch mTempoAlertSwitcher = (Switch) settingDialogContent.findViewById(R.id.dialog_setting_activity_switch_tempo);
 
-        final TextView counter = (TextView) dialogContent.findViewById(R.id.dialog_start_activity_text_counter);
-        final ProgressBar progressBar = (ProgressBar) dialogContent.findViewById(R.id.dialog_start_activity_progressbar_circle);
-        startDialog.create();
-        startDialog.show();
+        final EditText mDistanceAlertEditText = (EditText) settingDialogContent.findViewById(R.id.dialog_setting_activity_edittext_distance);
+        final EditText mTimeAlertEditText = (EditText) settingDialogContent.findViewById(R.id.dialog_setting_activity_edittext_time);
+        final EditText mTempoAlertEditText = (EditText) settingDialogContent.findViewById(R.id.dialog_setting_activity_edittext_tempo);
 
-        new CountDownTimer(6000, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                counter.setText(""+millisUntilFinished/1000);;
+        mDistanceAlertSwitcher.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mDistanceAlertEditText.setEnabled(isChecked);
+            }
+        });
+        mTimeAlertSwitcher.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mTimeAlertEditText.setEnabled(isChecked);
+            }
+        });
+        mTempoAlertSwitcher.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mTempoAlertEditText.setEnabled(isChecked);
+            }
+        });
+
+
+        final AlertDialog settingActivityDialog = new AlertDialog.Builder(this)
+                .setTitle(getString(R.string.track_activity_dialog_setting_title))
+                .setView(settingDialogContent)
+                .setPositiveButton(getString(R.string.track_activity_dialog_setting_positive),
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+
+                                if(!mDistanceAlertEditText.getText().toString().equals("")) {
+                                    mAlertDistanceInterval = Integer.parseInt(mDistanceAlertEditText.getText().toString());
+                                }
+                                if(!mTimeAlertEditText.getText().toString().equals("")) {
+                                    mAlertTimeInterval = Integer.parseInt(mTimeAlertEditText.getText().toString());
+                                }
+                                if(!mTempoAlertEditText.getText().toString().equals("")) {
+                                    mAlertTempoInterval = Integer.parseInt(mTempoAlertEditText.getText().toString());
+                                }
+
+                                View startDialogContent = getLayoutInflater().inflate(R.layout.dialog_start_activity, null);
+                                final DonutProgress progressBar = (DonutProgress) startDialogContent.findViewById(R.id.donut_progress);
+                                final AlertDialog startActivityDialog = new AlertDialog.Builder(ctx)
+                                        .setIcon(android.R.drawable.btn_star_big_on)
+                                        .setTitle(getString(R.string.track_activity_dialog_start_activity_title))
+                                        .setView(startDialogContent)
+                                        .create();
+                                startActivityDialog.show();
+
+                                new CountDownTimer(5000, 30) {
+                                    @Override
+                                    public void onTick(long millisUntilFinished) {
+                                        progressBar.setText(""+(millisUntilFinished/1000+1));
+                                        progressBar.setProgress((int)millisUntilFinished/50);
+                                    }
+
+                                    @Override
+                                    public void onFinish() {
+                                        startActivityDialog.dismiss();
+                                        mStartButton.setBackgroundColor(R.color.RED_LIGHT);
+                                        mTTS.speak("We start training!", TextToSpeech.QUEUE_FLUSH, null);
+
+                                        mDistance = 0;
+                                        mDistanceText.setText(String.format("%.2f", mDistance));
+                                        mTempo = 0;
+
+                                        mAlertDistanceStep = 0;
+                                        mAlertTimeStep = 0;
+                                        mAlertTempoStep = 0;
+
+                                        isTracked = true;
+
+                                        mChronometer.setBase(SystemClock.elapsedRealtime());
+                                        mChronometer.start();
+                                    }
+                                }.start();
+                            }
+                        }
+                )
+                .setNegativeButton(getString(R.string.track_activity_dialog_setting_negative),
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                dialog.dismiss();
+                            }
+                        }
+                )
+                .create();
+        settingActivityDialog.show();
+    }
+
+
+    public static String getOrdinalFromNumber(int i) {
+        String[] sufixes = new String[] { "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th" };
+        switch (i % 100) {
+            case 11:
+            case 12:
+            case 13:
+                return i + "th";
+            default:
+                return i + sufixes[i % 10];
+
+        }
+    }
+
+    @Override
+    public void onInit(int status) {
+        // TODO Auto-generated method stub
+        if (status == TextToSpeech.SUCCESS) {
+
+            Locale locale = new Locale("en");
+
+            int result = mTTS.setLanguage(locale);
+            mTTS.setLanguage(Locale.US);
+            //int result = mTTS.setLanguage(Locale.getDefault());
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                speechIsAvailable = false;
+                Log.e("RUN-BOY-RUN", "TTS Извините, этот язык не поддерживается");
+            } else {
+                speechIsAvailable = true;
             }
 
-            @Override
-            public void onFinish() {
+        } else {
+            Log.e("RUN-BOY-RUN", "TTS Ошибка!");
+        }
 
-            }
-        }.start();
     }
 
 
     @Override
     protected void onDestroy() {
+        // Don't forget to shutdown mTTS!
+        if (mTTS != null) {
+            mTTS.stop();
+            mTTS.shutdown();
+        }
         super.onDestroy();
-        scheduleTaskExecutor.shutdown();
-        scheduleTaskExecutor.shutdownNow();
-        scheduleTaskExecutor = null;
     }
 }
