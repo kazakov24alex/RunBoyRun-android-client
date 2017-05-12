cd6ca225cc077e27de8df468b01c2ad5b14411ea
kazakov24alex <kazakov24alex@gmail.com>
2017-05-12 16:32:40 +0300

    StartNewActivity: sport selector was added

--- app/src/main/java/twoAK/runboyrun/activities/StartNewActivityActivity.java
+++ app/src/main/java/twoAK/runboyrun/activities/StartNewActivityActivity.java
@@ -1,17 +1,43 @@
 package twoAK.runboyrun.activities;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewStub;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import twoAK.runboyrun.R;
+import twoAK.runboyrun.adapters.SquareImageView;
 
 public class StartNewActivityActivity extends BaseActivity {
+
+    static final String APP_TAG = "RUN-BOY-RUN";
+    static final String ACTIVITY_TAG = "["+ConditionActivity.class.getName()+"]: ";
+
+    static final String ENUM_SPORT_RUNNING = "RUNNING";
+    static final String ENUM_SPORT_CYCLING = "CYCLING";
+    static final String ENUM_SPORT_WALKING = "WALKING";
+    static final String ENUM_SPORT_SKIRUN  = "SKIRUN";
+
+    private int mSportFlag;
+    private String mSportValue;
+    private List<SquareImageView> mSportButtonsList;
+
+    private SquareImageView mSportRunningButton;
+    private SquareImageView mSportCyclingButton;
+    private SquareImageView mSportWalkingButton;
+    private SquareImageView mSportSkirunButton;
+
+    private TextView mSportRunningText;
+    private TextView mSportCyclingText;
+    private TextView mSportWalkingText;
+    private TextView mSportSkirunText;
 
 
     @Override
@@ -23,19 +49,40 @@ public class StartNewActivityActivity extends BaseActivity {
         stub.setLayoutResource(R.layout.content_start_activity);
         View inflated = stub.inflate();
 
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "StartNewActivityActivity", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
         //Set nav drawer selected to first item in list
         mNavigationView.getMenu().getItem(0).setChecked(true);
+
+
+        mSportFlag = 0;
+        mSportValue = "";
+        mSportButtonsList = new ArrayList<SquareImageView>();
+
+        // Initialization images of button panel
+        mSportRunningButton = (SquareImageView) findViewById(R.id.start_activity_imagebutton_sport_running);
+        mSportRunningButton.setImageResource(R.drawable.com_facebook_profile_picture_blank_square);
+        mSportButtonsList.add(mSportRunningButton);
+        mSportCyclingButton = (SquareImageView) findViewById(R.id.start_activity_imagebutton_sport_cycling);
+        mSportCyclingButton.setImageResource(R.drawable.com_facebook_profile_picture_blank_square);
+        mSportButtonsList.add(mSportCyclingButton);
+        mSportWalkingButton = (SquareImageView) findViewById(R.id.start_activity_imagebutton_sport_walking);
+        mSportWalkingButton.setImageResource(R.drawable.com_facebook_profile_picture_blank_square);
+        mSportButtonsList.add(mSportWalkingButton);
+        mSportSkirunButton = (SquareImageView) findViewById(R.id.start_activity_imagebutton_sport_skirun);
+        mSportSkirunButton.setImageResource(R.drawable.com_facebook_profile_picture_blank_square);
+        mSportButtonsList.add(mSportSkirunButton);
+
+        // initialization text of button panel and setting custom font
+        Typeface squareFont = Typeface.createFromAsset(getAssets(), "fonts/square.ttf");
+
+        mSportRunningText = (TextView) findViewById(R.id.start_activity_text_sport_running);
+        mSportRunningText.setTypeface(squareFont);
+        mSportCyclingText = (TextView) findViewById(R.id.start_activity_text_sport_cycling);
+        mSportCyclingText.setTypeface(squareFont);
+        mSportWalkingText = (TextView) findViewById(R.id.start_activity_text_sport_walking);
+        mSportWalkingText.setTypeface(squareFont);
+        mSportSkirunText = (TextView) findViewById(R.id.start_activity_text_sport_skirun);
+        mSportSkirunText.setTypeface(squareFont);
+
     }
 
     @Override
@@ -71,5 +118,37 @@ public class StartNewActivityActivity extends BaseActivity {
             default:
                 break;
         }
+    }
+
+
+    public void onSportSelectorClick(View view) {
+        if(mSportFlag != -1) {
+            mSportButtonsList.get(mSportFlag).setBackgroundResource(0);
+        }
+
+        switch(view.getId()) {
+            case R.id.start_activity_imagebutton_sport_running:
+                mSportFlag = 0;
+                mSportValue = ENUM_SPORT_RUNNING;
+                break;
+            case R.id.start_activity_imagebutton_sport_cycling:
+                mSportFlag = 1;
+                mSportValue = ENUM_SPORT_CYCLING;
+                break;
+            case R.id.start_activity_imagebutton_sport_walking:
+                mSportFlag = 2;
+                mSportValue = ENUM_SPORT_WALKING;
+                break;
+            case R.id.start_activity_imagebutton_sport_skirun:
+                mSportFlag = 3;
+                mSportValue = ENUM_SPORT_SKIRUN;
+                break;
+            default:
+                mSportFlag = -1;
+                mSportValue = "";
+                break;
+        }
+
+        view.setBackgroundResource(R.color.IMAGE_BUTTON_SELECT);
     }
 }
