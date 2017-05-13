55a6f7197e28afdc9f05dbd71e427a474ddfa933
kazakov24alex <kazakov24alex@gmail.com>
2017-05-13 20:12:50 +0300

    StartNewActivity: distance and time pickers was added

--- app/src/main/java/twoAK/runboyrun/activities/EnterCompletedActivity.java
+++ app/src/main/java/twoAK/runboyrun/activities/EnterCompletedActivity.java
@@ -5,9 +5,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewStub;
 
+import java.util.Calendar;
+
+import antistatic.spinnerwheel.AbstractWheel;
+import antistatic.spinnerwheel.OnWheelChangedListener;
+import antistatic.spinnerwheel.OnWheelClickedListener;
+import antistatic.spinnerwheel.OnWheelScrollListener;
+import antistatic.spinnerwheel.adapters.NumericWheelAdapter;
 import twoAK.runboyrun.R;
 
 public class EnterCompletedActivity extends BaseActivity {
+    // Time changed flag
+    private boolean timeChanged = false;
+
+    // Time scrolled flag
+    private boolean timeScrolled = false;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +32,95 @@ public class EnterCompletedActivity extends BaseActivity {
         stub.setLayoutResource(R.layout.content_enter_completed);
         View inflated = stub.inflate();
 
+
+        final AbstractWheel hourses = (AbstractWheel) findViewById(R.id.hour_horizontal);
+        NumericWheelAdapter hourAdapter = new NumericWheelAdapter(this, 0,1000, "%03d");
+        hourAdapter.setItemResource(R.layout.wheel_text_centered);
+        hourAdapter.setItemTextResource(R.id.text);
+        hourses.setViewAdapter(hourAdapter);
+
+        final AbstractWheel minses = (AbstractWheel) findViewById(R.id.mins);
+        NumericWheelAdapter minAdapter = new NumericWheelAdapter(this, 0, 10, "%03d");
+        minAdapter.setItemResource(R.layout.wheel_text_centered_dark_back);
+        minAdapter.setItemTextResource(R.id.text);
+        minses.setViewAdapter(minAdapter);
+
+
+
+
+        final AbstractWheel hours = (AbstractWheel) findViewById(R.id.hour);
+        hours.setViewAdapter(new NumericWheelAdapter(this, 0, 23));
+        hours.setCyclic(true);
+
+        final AbstractWheel mins = (AbstractWheel) findViewById(R.id.minutes);
+        mins.setViewAdapter(new NumericWheelAdapter(this, 0, 59, "%02d"));
+        mins.setCyclic(true);
+
+
+        // set current time
+        Calendar c = Calendar.getInstance();
+        int curHours = c.get(Calendar.HOUR_OF_DAY);
+        int curMinutes = c.get(Calendar.MINUTE);
+
+        hours.setCurrentItem(curHours);
+        mins.setCurrentItem(curMinutes);
+
+        // add listeners
+        addChangingListener(mins, "min");
+        addChangingListener(hours, "hour");
+
+        OnWheelChangedListener wheelListener = new OnWheelChangedListener() {
+            public void onChanged(AbstractWheel wheel, int oldValue, int newValue) {
+                if (!timeScrolled) {
+                    timeChanged = true;
+                    timeChanged = false;
+                }
+            }
+        };
+        hours.addChangingListener(wheelListener);
+        mins.addChangingListener(wheelListener);
+
+        OnWheelClickedListener click = new OnWheelClickedListener() {
+            public void onItemClicked(AbstractWheel wheel, int itemIndex) {
+                wheel.setCurrentItem(itemIndex, true);
+            }
+        };
+        hours.addClickingListener(click);
+        mins.addClickingListener(click);
+
+        OnWheelScrollListener scrollListener = new OnWheelScrollListener() {
+            public void onScrollingStarted(AbstractWheel wheel) {
+                timeScrolled = true;
+            }
+            public void onScrollingFinished(AbstractWheel wheel) {
+                timeScrolled = false;
+                timeChanged = true;
+                timeChanged = false;
+            }
+        };
+
+        hours.addScrollingListener(scrollListener);
+        mins.addScrollingListener(scrollListener);
+
+
+
+
+
     }
 
+
+
+    /**
+     * Adds changing listener for spinnerwheel that updates the spinnerwheel label
+     * @param wheel the spinnerwheel
+     * @param label the spinnerwheel label
+     */
+    private void addChangingListener(final AbstractWheel wheel, final String label) {
+        wheel.addChangingListener(new OnWheelChangedListener() {
+            public void onChanged(AbstractWheel wheel, int oldValue, int newValue) {
+                //spinnerwheel.setLabel(newValue != 1 ? label + "s" : label);
+            }
+        });
+    }
 
 }
