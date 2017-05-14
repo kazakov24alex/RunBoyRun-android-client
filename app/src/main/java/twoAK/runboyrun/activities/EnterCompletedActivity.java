b07a7213b78ac6f959e60fdc0484603b21eedcea
kazakov24alex <kazakov24alex@gmail.com>
2017-05-14 03:14:08 +0300

    EnterCompletedActivity: working version

--- app/src/main/java/twoAK/runboyrun/activities/EnterCompletedActivity.java
+++ app/src/main/java/twoAK/runboyrun/activities/EnterCompletedActivity.java
@@ -1,9 +1,16 @@
 package twoAK.runboyrun.activities;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewStub;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import antistatic.spinnerwheel.AbstractWheel;
 import twoAK.runboyrun.R;
@@ -11,6 +18,12 @@ import twoAK.runboyrun.adapters.MetersWheelAdapter;
 import twoAK.runboyrun.adapters.TimeWheelAdapter;
 
 public class EnterCompletedActivity extends BaseActivity {
+
+    static final String APP_TAG = "RUN-BOY-RUN";
+    static final String ACTIVITY_TAG = "["+ConditionActivity.class.getName()+"]: ";
+
+    private TimePicker mTimePicker;
+    private DatePicker mDatePicker;
 
     private AbstractWheel mKilometersWheel;
     private AbstractWheel mMetersWheel;
@@ -29,50 +42,90 @@ public class EnterCompletedActivity extends BaseActivity {
         View inflated = stub.inflate();
 
 
-        mKilometersWheel = (AbstractWheel) findViewById(R.id.hour_horizontal);
+        mTimePicker = (TimePicker) findViewById(R.id.enter_completed_timepicker_starttime);
+        mDatePicker = (DatePicker) findViewById(R.id.enter_completed_datepicker_starttime);
+
+        // Kilometers horizontal wheel
+        mKilometersWheel = (AbstractWheel) findViewById(R.id.enter_completed_wheel_km);
         antistatic.spinnerwheel.adapters.TimeAdapter kilometersAdapter = new antistatic.spinnerwheel.adapters.TimeAdapter(this, 1,100, "%d");
         kilometersAdapter.setItemResource(R.layout.wheel_text_centered);
         kilometersAdapter.setItemTextResource(R.id.text);
         mKilometersWheel.setViewAdapter(kilometersAdapter);
+        mKilometersWheel.setCurrentItem(2);
 
-        mMetersWheel = (AbstractWheel) findViewById(R.id.mins);
+        // Meters horizontal wheel
+        mMetersWheel = (AbstractWheel) findViewById(R.id.enter_completed_wheel_meters);
         MetersWheelAdapter metersAdapter = new MetersWheelAdapter(this, 1, 20, "%d");
         metersAdapter.setItemResource(R.layout.wheel_text_centered_dark_back);
         metersAdapter.setItemTextResource(R.id.text);
         mMetersWheel.setViewAdapter(metersAdapter);
+        mMetersWheel.setCurrentItem(2);
 
-
-
-
-        mHoursWheel = (AbstractWheel) findViewById(R.id.hour);
-        mHoursWheel.setViewAdapter(new TimeWheelAdapter(this, 23, "hours"));
+        // Hours vertical wheel
+        mHoursWheel = (AbstractWheel) findViewById(R.id.enter_completed_wheel_hours);
+        mHoursWheel.setViewAdapter(new TimeWheelAdapter(this, 23, getString(R.string.enter_completed_text_label_hours)));
         mHoursWheel.setVisibleItems(4);
         mHoursWheel.setCyclic(false);
         mHoursWheel.setCurrentItem(1);
 
-
-        mMinutesWheel = (AbstractWheel) findViewById(R.id.minutes);
-        mMinutesWheel.setViewAdapter(new TimeWheelAdapter(this, 60, "min"));
+        // Minutes vertical wheel
+        mMinutesWheel = (AbstractWheel) findViewById(R.id.enter_completed_wheel_minutes);
+        mMinutesWheel.setViewAdapter(new TimeWheelAdapter(this, 60, getString(R.string.enter_completed_text_label_minutes)));
         mMinutesWheel.setVisibleItems(4);
         mMinutesWheel.setCyclic(false);
         mMinutesWheel.setCurrentItem(15);
 
-
-
-       /* OnWheelClickedListener click = new OnWheelClickedListener() {
-            public void onItemClicked(AbstractWheel wheel, int itemIndex) {
-                wheel.setCurrentItem(itemIndex, true);
-            }
-        };
-        mHoursWheel.addClickingListener(click);
-        mMinutesWheel.addClickingListener(click);*/
-
-
-
-
-
     }
 
+
+    public void onRecordButtonClick(View view) {
+        // check distance
+        int distance = (mKilometersWheel.getCurrentItem()+50)*1000 + (mMetersWheel.getCurrentItem()+1)*50;
+        if(distance < 1000) {
+            Toast.makeText(this, getString(R.string.enter_completed_toast_error_short_distance), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // check spent time
+        int spentTime = 1000*60*60*mHoursWheel.getCurrentItem()+1000*60*mMinutesWheel.getCurrentItem();
+        if(spentTime < (1000*60*10)) {
+            Toast.makeText(this, getString(R.string.enter_completed_toast_error_short_activity), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // check start time
+        Calendar calendar = new GregorianCalendar(
+                mDatePicker.getYear(),
+                mDatePicker.getMonth(),
+                mDatePicker.getDayOfMonth(),
+                mTimePicker.getCurrentHour(),
+                mTimePicker.getCurrentMinute());
+        long setTime = calendar.getTimeInMillis();
+
+        calendar = new GregorianCalendar();
+        long curTime = calendar.getTimeInMillis();
+
+        long weekInMiliSec = 1000*60*60*24*7;
+
+        if( (curTime-setTime) > weekInMiliSec ) {
+            Toast.makeText(this, getString(R.string.enter_completed_toast_error_past), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(curTime < setTime) {
+            Toast.makeText(this, getString(R.string.enter_completed_toast_error_future), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        // send data to condition activity
+        Intent intent = new Intent(EnterCompletedActivity.this, ConditionActivity.class);
+        intent.putExtra("DISTANCE", distance);
+        intent.putExtra("START_TIME", setTime);
+        intent.putExtra("SPENT_TIME", spentTime);
+        startActivity(intent);
+
+    }
 
 
 }
