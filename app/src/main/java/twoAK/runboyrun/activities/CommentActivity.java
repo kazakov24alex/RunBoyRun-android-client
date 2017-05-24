32ddc63041d273db8d7a662d2c11b3c89518374d
kazakov24alex <kazakov24alex@gmail.com>
2017-05-24 19:57:10 +0300

    CommentsActivity - the advent of paggination

--- app/src/main/java/twoAK/runboyrun/activities/CommentActivity.java
+++ app/src/main/java/twoAK/runboyrun/activities/CommentActivity.java
@@ -1,11 +1,15 @@
 package twoAK.runboyrun.activities;
 
 
+import android.content.Context;
+import android.graphics.PointF;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.LinearSmoothScroller;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewStub;
@@ -14,6 +18,9 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
+import antistatic.spinnerwheel.AbstractWheel;
+import antistatic.spinnerwheel.OnWheelScrollListener;
+import antistatic.spinnerwheel.adapters.TimeAdapter;
 import twoAK.runboyrun.R;
 import twoAK.runboyrun.adapters.CustomDividerItemDecoration;
 import twoAK.runboyrun.adapters.DataAdapter;
@@ -29,14 +36,18 @@ public class CommentActivity extends BaseActivity {
     static final String APP_TAG = "RUN-BOY-RUN";
     static final String ACTIVITY_TAG = "[" + ConditionActivity.class.getName() + "]: ";
 
-    static final int COMMENTS_PER_PAGE = 6;
+    static final int COMMENTS_PER_PAGE = 8;
 
     private int mAllCommentsNum;
     private int mLastLoadedPage;
 
     private int mActivityID;
+    private boolean isPageScroll = false;
+    private int lastPage = 10;
 
     private GetCommentsPageTask mGetCommentsPageTask;
+
+    private AbstractWheel mPagesWheel;
 
     private RecyclerView mRecyclerView;
     private LinearLayoutManager mLinearLayoutManager;
@@ -71,18 +82,68 @@ public class CommentActivity extends BaseActivity {
 
         handler = new Handler();
 
+
+        // Meters horizontal wheel
+        mPagesWheel = (AbstractWheel) findViewById(R.id.comment_activity_wheel_meters);
+        TimeAdapter metersAdapter = new TimeAdapter(this, 1, 1, "%d");
+        metersAdapter.setItemResource(R.layout.wheel_text_centered_dark_back);
+        metersAdapter.setItemTextResource(R.id.text);
+        mPagesWheel.setViewAdapter(metersAdapter);
+        mPagesWheel.setCurrentItem(2);
+
+        /*OnWheelChangedListener wheelListener = new OnWheelChangedListener() {
+            public void onChanged(AbstractWheel wheel, int oldValue, int newValue) {
+                Log.i(APP_TAG, ACTIVITY_TAG + "PAGE = "+newValue);
+
+                int totalVisibleItems = mLinearLayoutManager.findLastVisibleItemPosition() - mLinearLayoutManager.findFirstVisibleItemPosition();
+                int centeredItemPosition = totalVisibleItems / 2;
+                mRecyclerView.smoothScrollToPosition(newValue*COMMENTS_PER_PAGE);
+
+
+
+               // mLinearLayoutManager.scrollToPosition(newValue*COMMENTS_PER_PAGE + 2);
+            }
+        };
+        mPagesWheel.addChangingListener(wheelListener);*/
+
+
+        OnWheelScrollListener scrollListener = new OnWheelScrollListener() {
+            public void onScrollingStarted(AbstractWheel wheel) {
+                isPageScroll = true;
+            }
+            public void onScrollingFinished(AbstractWheel wheel) {
+                isPageScroll = false;
+
+                int currentPage = mPagesWheel.getCurrentItem();
+                int totalVisibleItems = mLinearLayoutManager.findLastVisibleItemPosition() - mLinearLayoutManager.findFirstVisibleItemPosition() - 1;
+
+                if( (lastPage > currentPage) || ( (currentPage*COMMENTS_PER_PAGE + totalVisibleItems) > mAllCommentsNum) )  {
+                    Log.i(APP_TAG, ACTIVITY_TAG + "NO");
+                    mRecyclerView.scrollToPosition(mPagesWheel.getCurrentItem()*COMMENTS_PER_PAGE);
+
+                } else {
+                    mRecyclerView.scrollToPosition(mPagesWheel.getCurrentItem()*COMMENTS_PER_PAGE + totalVisibleItems);
+                }
+
+                lastPage = mPagesWheel.getCurrentItem();
+              }
+        };
+        mPagesWheel.addScrollingListener(scrollListener);
+
+
+
         mRecyclerView = (RecyclerView) findViewById(R.id.comment_activity_recycler_view);
 
-        mLinearLayoutManager = new LinearLayoutManager(this);
+        mLinearLayoutManager = new MyCustomLayoutManager(this);
         mRecyclerView.setLayoutManager(mLinearLayoutManager);
         mRecyclerView.addItemDecoration(new CustomDividerItemDecoration(this));
+
 
         mCommentsList = new ArrayList<CommentObject>();
         mCommentRecycleAdapter = new DataAdapter(mCommentsList, mRecyclerView);
         mCommentRecycleAdapter.setOnLoadMoreListener(new OnLoadMoreListener() {
             @Override
             public void onLoadMore() {
-
                 if(mCommentsList.size() < mAllCommentsNum) {
                     //add null , so the adapter will check view_type and show progress bar at bottom
                     mCommentsList.add(null);
@@ -105,6 +166,18 @@ public class CommentActivity extends BaseActivity {
             }
         });
         mRecyclerView.setAdapter(mCommentRecycleAdapter);
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                Log.i(APP_TAG, ACTIVITY_TAG + "SCROLL 1");
+                if(!isPageScroll) {
+
+                    int currentPage = (mLinearLayoutManager.findLastVisibleItemPosition() + 1) / COMMENTS_PER_PAGE;
+                    mPagesWheel.setCurrentItem(currentPage);
+                    Log.i(APP_TAG, ACTIVITY_TAG + "SCROLL 2 PAGE= "+currentPage);
+                }
+            }
+        });
 
 
         mGetCommentsPageTask = new GetCommentsPageTask(mActivityID, COMMENTS_PER_PAGE, ++mLastLoadedPage);
@@ -112,6 +185,45 @@ public class CommentActivity extends BaseActivity {
 
     }
 
+
+    public class MyCustomLayoutManager extends LinearLayoutManager {
+        private static final float MILLISECONDS_PER_INCH = 50f;
+        private Context mContext;
+
+        public MyCustomLayoutManager(Context context) {
+            super(context);
+            mContext = context;
+        }
+
+        @Override
+        public void smoothScrollToPosition(RecyclerView recyclerView,
+                                           RecyclerView.State state, final int position) {
+
+            LinearSmoothScroller smoothScroller =
+                    new LinearSmoothScroller(mContext) {
+
+                        //This controls the direction in which smoothScroll looks
+                        //for your view
+                        @Override
+                        public PointF computeScrollVectorForPosition
+                        (int targetPosition) {
+                            return MyCustomLayoutManager.this
+                                    .computeScrollVectorForPosition(targetPosition);
+                        }
+
+                        //This returns the milliseconds it takes to
+                        //scroll one pixel.
+                        @Override
+                        protected float calculateSpeedPerPixel
+                        (DisplayMetrics displayMetrics) {
+                            return MILLISECONDS_PER_INCH/displayMetrics.densityDpi;
+                        }
+                    };
+
+            smoothScroller.setTargetPosition(position);
+            startSmoothScroll(smoothScroller);
+        }
+    }
 
     private class GetCommentsPageTask extends AsyncTask<Void, Void, GetCommentsResponse> {
         private String errMes;  // error message possible
@@ -152,6 +264,12 @@ public class CommentActivity extends BaseActivity {
             } else {
                 if (mAllCommentsNum == -1) {
                     mAllCommentsNum = commentsResponse.getComments().get(0).getOrder();
+
+                    int pagesNum = mAllCommentsNum / COMMENTS_PER_PAGE + 1;
+                    TimeAdapter metersAdapter = new TimeAdapter(getApplicationContext(), 1, pagesNum, "%d");
+                    metersAdapter.setItemResource(R.layout.wheel_text_centered_dark_back);
+                    metersAdapter.setItemTextResource(R.id.text);
+                    mPagesWheel.setViewAdapter(metersAdapter);
                 }
 
                 for (int i = 0; i < commentsResponse.getComments().size(); i++) {
