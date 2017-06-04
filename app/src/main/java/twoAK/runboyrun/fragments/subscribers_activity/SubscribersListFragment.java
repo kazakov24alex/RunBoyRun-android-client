164a5c7f11bc2ae45377badf880fe2fd8c0620b4
kazakov24alex <kazakov24alex@gmail.com>
2017-06-04 14:49:07 +0300

    Commit

--- app/src/main/java/twoAK/runboyrun/fragments/subscribers_activity/SubscribersListFragment.java
+++ app/src/main/java/twoAK/runboyrun/fragments/subscribers_activity/SubscribersListFragment.java
@@ -34,7 +34,6 @@ public class SubscribersListFragment extends Fragment {
         View mRootView = inflater.inflate(R.layout.fragment_like_list, container, false);
 
         mListView = (ListView) mRootView.findViewById(R.id.like_list_listview);
-
         // on item click listener
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
